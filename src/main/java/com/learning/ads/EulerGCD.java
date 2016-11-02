4aafc85d9dc44074873e1b61d979e4a6fe36daed
Arun Rahul <rahul.arun86@gmail.com>
2016-11-02 12:03:29 +0530

    Recursive Euclidean algorithm to find out gcd

--- src/main/java/com/learning/ads/EulerGCD.java
+++ src/main/java/com/learning/ads/EulerGCD.java
@@ -1,9 +1,22 @@
 package com.learning.ads;
 
 public class EulerGCD {
+	
+	/**
+	 * A recursive implementation of EULER GCD algorithm
+	 * @param a
+	 * @param b
+	 * @return gcd of a and b
+	 */
+	public static int gcd(int a, int b){
+		if(b == 0){
+			return a;
+		}
+		return gcd(b, a%b);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(gcd(1071,462));
 
 	}
 
