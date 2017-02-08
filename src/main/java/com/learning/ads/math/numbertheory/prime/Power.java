fb719109a048284fe3ed4a1c0b8bdddfc08b3c28
Arun Rahul <rahul.arun86@gmail.com>
2017-02-08 07:49:02 +0530

    Calculating power

---
+++ src/main/java/com/learning/ads/math/numbertheory/prime/Power.java
@@ -0,0 +1,25 @@
+package com.learning.ads.math.numbertheory.prime;
+
+public class Power {
+	/*
+	 *   T(n) = k            if n is 1
+	 *   T(n) = k * T(n-1)   if n is odd
+	 *   T(n) = square(T(n/2))       if n is even 
+	 */
+	public static long power(int variable, int exponent){
+		if(exponent == 1){
+			return variable;
+		}
+		if((exponent & 1) == 1){ //even exponent
+			return variable * power(variable, exponent-1);
+		}else{
+			long intProduct = power(variable, exponent/2);
+			return intProduct * intProduct;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(power(2,5));
+	}
+
+}
