b3dc6721f0f9bda335bc5e8dfc7a97c179e172ca
larunrahul <rahul.arun86@gmail.com>
2017-05-30 17:24:56 +0530

    Refactored maxsubarray and added inefficient polynomial solution. Also added corresponding tests

--- src/main/java/com/learning/ads/divideandconquer/MaximumSubarray.java
+++ src/main/java/com/learning/ads/divideandconquer/MaximumSubarray.java
@@ -7,6 +7,57 @@ public class MaximumSubarray {
 		this.array = array;
 	}
 
+	/**
+	 * This runs in {@code O(nlogn)}.
+	 * @return An array containing indices that form the max sum and max sum itself. 
+	 */
+	public int[] findMaximumSubarray() {
+		return findMaximumSubarray(0, array.length - 1);
+	}
+	
+	/**
+	 * This runs in {@code O(n}<sup>{@code 2}</sup>{@code )}.
+	 * @return An array containing indices that form the max sum and max sum itself. 
+	 */
+	public int[] findMaximumSubarrayInefficient() {
+		int min = 0, max = 0, sum = 0, maxSum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum = array[i];
+			for (int j = i + 1; j < array.length; j++) {
+				sum = sum + array[j];
+				if(sum > maxSum){
+					min = i;
+					max = j;
+					maxSum = sum;
+				}
+			}
+		}
+		return new int[] { min, max, maxSum };
+	}
+
+	private int[] findMaximumSubarray(int low, int high) {
+		if (high == low) {
+			return new int[] { low, high, array[low] };
+		}
+		int mid = (low + high) / 2;
+		int[] left = findMaximumSubarray(low, mid);
+		int[] right = findMaximumSubarray(mid + 1, high);
+		int[] cross = maxCrossingSubArray(low, mid, high);
+		if (left[2] >= right[2] && left[2] >= cross[2]) {
+			return left;
+		} else if (right[2] >= left[2] && right[2] >= cross[2]) {
+			return right;
+		}
+		return cross;
+	}
+	
+	/**
+	 * This runs in {@code O(n)}
+	 * @param low
+	 * @param mid
+	 * @param high
+	 * @return An array containing indices that form the max sum and max sum itself within given range.
+	 */
 	private int[] maxCrossingSubArray(int low, int mid, int high) {
 		int leftMaxSum = array[mid], leftMaxElement = mid, sum = leftMaxSum;
 		for (int i = mid - 1; i >= 0; i--) {
@@ -26,26 +77,6 @@ public class MaximumSubarray {
 			}
 		}
 		return new int[] { leftMaxElement, rightMaxElement, leftMaxSum + rightMaxSum };
-	}
-
-	public int[] findMaximumSubarray(int low, int high) {
-		if (high == low) {
-			return new int[] { low, high, array[low] };
-		}
-		int mid = (low + high) / 2;
-		int[] left = findMaximumSubarray(low, mid);
-		int[] right = findMaximumSubarray(mid + 1, high);
-		int[] cross = maxCrossingSubArray(low, mid, high);
-		if (left[2] >= right[2] && left[2] >= cross[2]) {
-			return left;
-		} else if (right[2] >= left[2] && right[2] >= cross[2]) {
-			return right;
-		}
-		return cross;
-	}
-
-	public static void main(String[] args) {
-		
 	}
 
 }
--- src/test/java/com/learning/ads/divideandconquer/MaximumSubarrayTest.java
+++ src/test/java/com/learning/ads/divideandconquer/MaximumSubarrayTest.java
@@ -7,10 +7,18 @@ import org.junit.Test;
 public class MaximumSubarrayTest {
 
 	@Test
-	public void test() {
+	public void maxSubarrayWithnlognSolution() {
 		int[] array = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
 		MaximumSubarray maximumSubarray = new MaximumSubarray(array);
-		int[] result = maximumSubarray.findMaximumSubarray(0, array.length - 1);
+		int[] result = maximumSubarray.findMaximumSubarray();
+		assertArrayEquals(new int[] { 7, 10, 43 }, result);
+	}
+	
+	@Test
+	public void maxSubarrayWithnsquaredSolution() {
+		int[] array = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+		MaximumSubarray maximumSubarray = new MaximumSubarray(array);
+		int[] result = maximumSubarray.findMaximumSubarrayInefficient();
 		assertArrayEquals(new int[] { 7, 10, 43 }, result);
 	}
 
