a3c742d5651e92b767e5d91f0e59b4fbe8bc966e
larunrahul <rahul.arun86@gmail.com>
2017-07-03 23:21:19 +0530

    Added more bit manipulation stuff

---
+++ src/main/java/com/learning/ads/bitmanipulation/EqualSumAndXOR.java
@@ -0,0 +1,10 @@
+package com.learning.ads.bitmanipulation;
+
+/**
+ * Problem : Given a positive integer n, find count of positive integers i such that 0 <= i <= n and n+i = n^i
+ * @author Arun Rahul
+ *
+ */
+public class EqualSumAndXOR {
+	
+}
--- src/main/java/com/learning/ads/bitmanipulation/SetBitCounter.java
+++ src/main/java/com/learning/ads/bitmanipulation/SetBitCounter.java
@@ -1,5 +1,11 @@
 package com.learning.ads.bitmanipulation;
 
+/**
+ * Resources used:
+ * http://www.geeksforgeeks.org/?p=1176
+ * @author Arun Rahul
+ *
+ */
 public class SetBitCounter {
 
 	/**
@@ -23,7 +29,7 @@ public class SetBitCounter {
 	
 	/**
 	 * Subtraction of 1 from a number toggles all the bits (from right to left) till the rightmost set bit(including the righmost set bit). 
-	 * So if we subtract a number by 1 and do bitwise & with itself (n & (n-1)), we unset the righmost set bit. 
+	 * So if we subtract a number by 1 and do bitwise & with itself (n & (n-1)), we unset the rightmost set bit. 
 	 * If we do n & (n-1) in a loop and count the no of times loop executes we get the set bit count.
 	 * Beauty of the this solution is number of times it loops is equal to the number of set bits in a given integer.
 	 * 
