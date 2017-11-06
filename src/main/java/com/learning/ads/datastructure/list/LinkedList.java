27e08ce5483b50d96d532e524b76089608d949b0
larunrahul <rahul.arun86@gmail.com>
2017-11-06 14:28:23 +0530

    Adding basic list and stack

---
+++ src/main/java/com/learning/ads/datastructure/list/LinkedList.java
@@ -0,0 +1,16 @@
+package com.learning.ads.datastructure.list;
+
+public class LinkedList {
+	
+	public Node head;
+	public Node tail;
+	public Node preTail;
+	
+	static public class Node{
+		public int value;
+		public Node next;
+	}
+	public LinkedList() {
+		
+	}
+}
---
+++ src/main/java/com/learning/ads/datastructure/stack/Stack.java
@@ -0,0 +1,5 @@
+package com.learning.ads.datastructure.stack;
+
+public class Stack {
+	
+}
