b42f4369993f022c758e9420a8fb5835dec27b9f
larunrahul <rahul.arun86@gmail.com>
2017-12-26 12:48:02 +0530

    Adding few more ways to flatten linked list

--- src/main/java/com/learning/ads/datastructure/list/problems/FlattenLinkedList.java
+++ src/main/java/com/learning/ads/datastructure/list/problems/FlattenLinkedList.java
@@ -1,35 +1,106 @@
 package com.learning.ads.datastructure.list.problems;
 
+/**
+ * Flattening a linked list
+ * 
+ * @author Arun Rahul
+ *
+ */
 public class FlattenLinkedList {
 
 	/**
-	 * Flattens multilevel linked list to a single level linked list. This doesn't disturb the original list.
-	 * @param head : List of type ListBiNode that has pointers to both next and branch
-	 * @return : List of type ListNode whic is flattened form of head
+	 * Flattens multilevel linked list to a single level linked list. This doesn't
+	 * disturb the original list.
+	 * 
+	 * @param head
+	 *            : List of type ListBiNode that has pointers to both next and
+	 *            branch
+	 * @return : List of type ListNode which is flattened form of head
 	 */
-	public ListNode flatten(ListBiNode head) {
+	public ListNode flattenRec(ListBiNode head) {
 		ListNode result = null;
 		if (head != null) {
 			result = new ListNode(head.value);
 			// A straight forward manipulation
 			if (head.down == null) {
-				result.next = flatten(head.next);
+				result.next = flattenRec(head.next);
 			}
 			// First go down to flatten it, then continue with next element
 			else {
 				ListBiNode next = head.next;
 				// Go down to flatten branch
-				result.next = flatten(head.down);
+				result.next = flattenRec(head.down);
 				ListNode end = result.next;
 				// traverse through the flatten till end
 				while (end.next != null) {
 					end = end.next;
 				}
 				// continue with next element
-				end.next = flatten(next);
+				end.next = flattenRec(next);
 			}
 		}
 		return result;
+	}
+
+	/**
+	 * Flattens multilevel linked list to a single level linked list. This doesn't
+	 * disturb the original list.
+	 * 
+	 * @param head
+	 *            : List of type ListBiNode that has pointers to both next and
+	 *            branch
+	 * @return : List of type ListNode which is flattened form of head
+	 */
+	public ListNode flatten(ListBiNode node) {
+		ListNode result = new ListNode(-1), tail = result;
+		ListBiNode head = node;
+		while (head != null) {
+			ListBiNode next = head.next;
+			// create new node for current node
+			tail.next = new ListNode(head.value);
+			tail = tail.next;
+			// if child, append it to the end of this list
+			if (head.down != null) {
+				head.next = head.down;
+				ListBiNode dNext = head.down;
+				while (dNext.next != null) {
+					dNext = dNext.next;
+				}
+				dNext.next = next;
+			}
+			head = head.next;
+		}
+
+		return result.next;
+	}
+
+	/**
+	 * Flatten a linked list level wise.
+	 * https://www.geeksforgeeks.org/flatten-a-linked-list-with-next-and-child-pointers/
+	 * 
+	 * @param node
+	 * @return
+	 */
+	public ListNode flattenLevelWise(ListBiNode node) {
+		ListNode result = new ListNode(-1), tail = result;
+		ListBiNode head = node, ltail = node;
+		// find the tail
+		while (ltail.next != null) {
+			ltail = ltail.next;
+		}
+		while (head != null) {
+			// new node for current node
+			tail.next = new ListNode(head.value);
+			tail = tail.next;
+			// append child to tail
+			ltail.next = head.down;
+			// find new tail
+			while (ltail.next != null) {
+				ltail = ltail.next;
+			}
+			head = head.next;
+		}
+		return result.next;
 	}
 
 }
--- src/test/java/com/learning/ads/datastructure/list/problems/FlattenLinkedListTest.java
+++ src/test/java/com/learning/ads/datastructure/list/problems/FlattenLinkedListTest.java
@@ -7,7 +7,23 @@ public class FlattenLinkedListTest {
 	FlattenLinkedList rll = new FlattenLinkedList();
 
 	@Test
-	public void multiBranch() {
+	public void flattenMultiBranchRec() {
+		ListBiNode firstBranch = new ListBiNode(3);
+		firstBranch.attach(4).attach(5);
+		ListBiNode subBranch = new ListBiNode(12);
+		subBranch.attach(13).attach(14).attach(15).attach(16);
+		ListBiNode secondBranch = new ListBiNode(9);
+		secondBranch.attach(10).attach(11).addBranch(subBranch).attach(17).attach(18);
+		ListBiNode head = new ListBiNode(1);
+		head.attach(2).addBranch(firstBranch).attach(6).attach(7).attach(8).addBranch(secondBranch);
+		ListNode expected = new ListNode(1);
+		expected.attach(2).attach(3).attach(4).attach(5).attach(6).attach(7).attach(8).attach(9).attach(10).attach(11)
+				.attach(12).attach(13).attach(14).attach(15).attach(16).attach(17).attach(18);
+		Util.assertListEqual(expected, rll.flattenRec(head));
+	}
+
+	@Test
+	public void flattenMultiBranch() {
 		ListBiNode firstBranch = new ListBiNode(3);
 		firstBranch.attach(4).attach(5);
 		ListBiNode subBranch = new ListBiNode(12);
@@ -23,12 +39,52 @@ public class FlattenLinkedListTest {
 	}
 
 	@Test
-	public void singleBranch() {
+	public void flattenLevelWiseMultiBranch() {
+		ListBiNode level31 = new ListBiNode(3);
+		ListBiNode level32 = new ListBiNode(19);
+		level32.attach(15);
+		ListBiNode level21 = new ListBiNode(2);
+		ListBiNode level22 = new ListBiNode(16);
+		level22.addBranch(level31);
+		ListBiNode level23 = new ListBiNode(9);
+		level23.addBranch(level32).attach(8);
+		ListBiNode level11 = new ListBiNode(4);
+		level11.attach(20).addBranch(level21).attach(13).addBranch(level22);
+		ListBiNode level12 = new ListBiNode(17);
+		level12.addBranch(level23).attach(6);
+		ListBiNode node = new ListBiNode(10);
+		node.addBranch(level11).attach(5).attach(12).attach(7).addBranch(level12).attach(11);
+		ListNode expected = new ListNode(10);
+		expected.attach(5).attach(12).attach(7).attach(11).attach(4).attach(20).attach(13).attach(17).attach(6)
+				.attach(2).attach(16).attach(9).attach(8).attach(3).attach(19).attach(15);
+		Util.assertListEqual(expected, rll.flattenLevelWise(node));
+	}
+
+	@Test
+	public void flattenSingleBranchRec() {
+		ListBiNode head = new ListBiNode(1);
+		head.attach(2).attach(6).attach(7).attach(8);
+		ListNode expected = new ListNode(1);
+		expected.attach(2).attach(6).attach(7).attach(8);
+		Util.assertListEqual(expected, rll.flattenRec(head));
+	}
+
+	@Test
+	public void flattenSingleBranch() {
 		ListBiNode head = new ListBiNode(1);
 		head.attach(2).attach(6).attach(7).attach(8);
 		ListNode expected = new ListNode(1);
 		expected.attach(2).attach(6).attach(7).attach(8);
 		Util.assertListEqual(expected, rll.flatten(head));
+	}
+
+	@Test
+	public void flattenLevelWiseSingleBranch() {
+		ListBiNode head = new ListBiNode(1);
+		head.attach(2).attach(6).attach(7).attach(8);
+		ListNode expected = new ListNode(1);
+		expected.attach(2).attach(6).attach(7).attach(8);
+		Util.assertListEqual(expected, rll.flattenLevelWise(head));
 	}
 
 }
