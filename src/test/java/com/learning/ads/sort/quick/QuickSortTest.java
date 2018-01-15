126781006862481a6c6b6fb12c97574129126e71
larunrahul <rahul.arun86@gmail.com>
2018-01-15 14:44:44 +0530

    Removing un-necessary print statement

--- src/test/java/com/learning/ads/sort/quick/QuickSortTest.java
+++ src/test/java/com/learning/ads/sort/quick/QuickSortTest.java
@@ -2,8 +2,6 @@ package com.learning.ads.sort.quick;
 
 import static org.junit.Assert.assertArrayEquals;
 
-import java.util.Arrays;
-
 import org.junit.Test;
 
 public class QuickSortTest {
@@ -23,7 +21,6 @@ public class QuickSortTest {
 		QuickSort<Integer, Partition<Integer>> sort = new QuickSort<>(p);
 		Integer[] array = { 2, 8, 7, 1, 3, 5, 6, 4 };
 		sort.sortDescending(array);
-		System.out.println(Arrays.toString(array));
 		assertArrayEquals(new Integer[] { 8, 7, 6, 5, 4, 3, 2, 1 }, array);
 	}
 
