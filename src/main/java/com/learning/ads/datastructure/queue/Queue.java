70625e6f5ed69b7af8f88b093077e0139824f522
larunrahul <rahul.arun86@gmail.com>
2018-03-29 12:45:39 +0530

    Adding dynamic queue implementation

---
+++ src/main/java/com/learning/ads/datastructure/queue/Queue.java
@@ -0,0 +1,118 @@
+package com.learning.ads.datastructure.queue;
+
+/**
+ * A basic queue implementation backed by dynamic circular array
+ * 
+ * @author Arun Rahul
+ *
+ */
+public class Queue<T> {
+
+	private Object[] array;
+
+	private int length;
+
+	private int tail;
+
+	private int head;
+
+	@SafeVarargs
+	public Queue(T... array) {
+		if (array.length == 0) {
+			this.array = new Object[1];
+		} else {
+			for (int i = 0; i < array.length / 2; i++) {
+				T temp = array[i];
+				array[i] = array[array.length - i - 1];
+				array[array.length - i - 1] = temp;
+			}
+			length = array.length;
+			this.array = array;
+			head = 0;
+			tail = array.length - 1;
+		}
+	}
+
+	public int length() {
+		return length;
+	}
+
+	public void enQueue(T element) {
+		if (isFull()) {
+			expand();
+		}
+		tail = (tail + 1) % array.length;
+		array[tail] = element;
+		length++;
+	}
+
+	public void deQueue() {
+		throwIfEmpty();
+		array[head] = null;
+		head = (head + 1) % array.length;
+		length--;
+	}
+
+	public T tail() {
+		throwIfEmpty();
+		return dataAt(tail);
+	}
+
+	public T head() {
+		throwIfEmpty();
+		return dataAt(head);
+	}
+
+	public boolean isFull() {
+		return length == array.length;
+	}
+
+	@SuppressWarnings("unchecked")
+	private T dataAt(int index) {
+		return (T) array[index];
+	}
+
+	private void throwIfEmpty() {
+		if (length == 0) {
+			throw new RuntimeException("Stack empty");
+		}
+	}
+
+	/**
+	 * Dynamic array. Each time when array is full, a new array of double the size
+	 * is created and old array is copied to new array
+	 */
+	private void expand() {
+		Object[] array = new Object[2 * this.array.length];
+		System.arraycopy(this.array, head, array, 0, length - head);
+		System.arraycopy(this.array, 0, array, length - head, head);
+		this.array = array;
+		head = 0;
+		tail = length - 1;
+	}
+
+	public T[] toArray() {
+		Object[] array = new Object[length];
+		System.arraycopy(this.array, head, array, 0, length - head);
+		System.arraycopy(this.array, 0, array, length - head, head);
+		return extracted(array);
+	}
+
+	@SuppressWarnings("unchecked")
+	private T[] extracted(Object[] array) {
+		return (T[]) array;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder("[");
+		for (int i = length - 1; i >= 0; i--) {
+			sb.append(array[i]).append(",");
+		}
+		sb.append("]");
+		if (sb.length() == 2) {
+			return sb.toString();
+		}
+		return sb.toString().substring(0, sb.length() - 2) + sb.toString().substring(sb.length() - 1, sb.length());
+	}
+
+}
---
+++ src/test/java/com/learning/ads/datastructure/queue/QueueTest.java
@@ -0,0 +1,123 @@
+package com.learning.ads.datastructure.queue;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class QueueTest {
+
+	Queue<Integer> queue = new Queue<>();
+
+	@Before
+	public void setUp() throws Exception {
+		queue = new Queue<>();
+		assertFalse(queue.isFull());
+		assertEquals(0, queue.length());
+	}
+
+	@Test
+	public void buldFilledQueue() {
+		queue = new Queue<>(10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+		assertEquals(10, queue.length());
+		assertEquals(Integer.valueOf(10), queue.tail());
+		assertEquals(Integer.valueOf(1), queue.head());
+		queue.deQueue();
+		assertEquals(9, queue.length());
+		assertEquals(Integer.valueOf(10), queue.tail());
+		assertEquals(Integer.valueOf(2), queue.head());
+		queue.enQueue(11);
+		assertEquals(10, queue.length());
+		assertEquals(Integer.valueOf(11), queue.tail());
+		assertEquals(Integer.valueOf(2), queue.head());
+		queue.enQueue(12);
+		assertEquals(11, queue.length());
+		assertEquals(Integer.valueOf(12), queue.tail());
+		assertEquals(Integer.valueOf(2), queue.head());
+	}
+
+	@Test
+	public void allOperations() {
+		queue.enQueue(1);
+		assertEquals(1, queue.length());
+		assertEquals(Integer.valueOf(1), queue.tail());
+		assertEquals(Integer.valueOf(1), queue.head());
+		queue.deQueue();
+		assertEquals(0, queue.length());
+		queue.enQueue(1);
+		assertEquals(1, queue.length());
+		assertEquals(Integer.valueOf(1), queue.tail());
+		assertEquals(Integer.valueOf(1), queue.head());
+		queue.enQueue(2);
+		assertEquals(2, queue.length());
+		assertEquals(Integer.valueOf(2), queue.tail());
+		assertEquals(Integer.valueOf(1), queue.head());
+		queue.deQueue();
+		assertEquals(1, queue.length());
+		assertEquals(Integer.valueOf(2), queue.tail());
+		assertEquals(Integer.valueOf(2), queue.head());
+		queue.deQueue();
+		assertEquals(0, queue.length());
+		queue.enQueue(1);
+		queue.enQueue(2);
+		assertEquals(2, queue.length());
+		assertEquals(Integer.valueOf(2), queue.tail());
+		assertEquals(Integer.valueOf(1), queue.head());
+		queue.deQueue();
+		assertEquals(1, queue.length());
+		assertEquals(Integer.valueOf(2), queue.tail());
+		assertEquals(Integer.valueOf(2), queue.head());
+		queue.deQueue();
+		assertEquals(0, queue.length());
+		queue.enQueue(1);
+		assertEquals(1, queue.length());
+		assertEquals(Integer.valueOf(1), queue.tail());
+		assertEquals(Integer.valueOf(1), queue.head());
+		queue.enQueue(2);
+		assertEquals(2, queue.length());
+		assertEquals(Integer.valueOf(2), queue.tail());
+		assertEquals(Integer.valueOf(1), queue.head());
+		queue.enQueue(3);
+		assertEquals(3, queue.length());
+		assertEquals(Integer.valueOf(3), queue.tail());
+		assertEquals(Integer.valueOf(1), queue.head());
+		queue.enQueue(4);
+		assertEquals(4, queue.length());
+		assertEquals(Integer.valueOf(4), queue.tail());
+		assertEquals(Integer.valueOf(1), queue.head());
+		queue.deQueue();
+		assertEquals(3, queue.length());
+		assertEquals(Integer.valueOf(4), queue.tail());
+		assertEquals(Integer.valueOf(2), queue.head());
+		queue.enQueue(5);
+		assertEquals(4, queue.length());
+		assertEquals(Integer.valueOf(5), queue.tail());
+		assertEquals(Integer.valueOf(2), queue.head());
+		queue.enQueue(6);
+		assertEquals(5, queue.length());
+		assertEquals(Integer.valueOf(6), queue.tail());
+		assertEquals(Integer.valueOf(2), queue.head());
+		queue.enQueue(7);
+		queue.enQueue(8);
+		queue.enQueue(9);
+		assertEquals(8, queue.length());
+		assertEquals(Integer.valueOf(9), queue.tail());
+		assertEquals(Integer.valueOf(2), queue.head());
+		queue.deQueue();
+		assertEquals(7, queue.length());
+		assertEquals(Integer.valueOf(9), queue.tail());
+		assertEquals(Integer.valueOf(3), queue.head());
+		queue.enQueue(10);
+		assertEquals(8, queue.length());
+		assertEquals(Integer.valueOf(10), queue.tail());
+		assertEquals(Integer.valueOf(3), queue.head());
+		queue.deQueue();
+		queue.enQueue(11);
+		assertEquals(8, queue.length());
+		assertEquals(Integer.valueOf(11), queue.tail());
+		assertEquals(Integer.valueOf(4), queue.head());
+		queue.enQueue(12);
+	}
+
+}
