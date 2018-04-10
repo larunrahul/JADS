358038c933663d6bc3326363c9b04cbee1b0e29c
larunrahul <rahul.arun86@gmail.com>
2018-04-10 23:10:14 +0530

    Adding KMP pattern matching algorithm

--- src/main/java/com/learning/ads/string/patternmatch/KMPPatternMatching.java
+++ src/main/java/com/learning/ads/string/patternmatch/KMPPatternMatching.java
@@ -9,8 +9,8 @@ public class KMPPatternMatching {
 	 * 
 	 * https://www.geeksforgeeks.org/searching-for-patterns-set-2-kmp-algorithm/
 	 * 
-	 * Complexity: O(n) + O(m) for creating lps array where n is length of text and
-	 * m is length of pattern
+	 * Complexity: O(n) (for comparison) + O(m) (for creating lps array) where n is
+	 * length of text and m is length of pattern
 	 * 
 	 * @param text
 	 *            Whole text in which we want to search for a pattern
@@ -20,16 +20,47 @@ public class KMPPatternMatching {
 	 * @return Index of the pattern in the text if matched, otherwise -1
 	 */
 	public int search(String text, String pattern) {
+		// O(m)
+		int[] lps = computeLPS(pattern);
+		int n = text.length();
+		int m = pattern.length();
+		int remainingLength = m;
+		int j = 0;
+		// O(n-m)
+		for (int i = 0; i <= n - m; i++) {
+			while (j < remainingLength && text.charAt(i + j) == pattern.charAt(j)) {
+				j++;
+			}
+			if (j == m) {
+				return i;
+			}
+			if (j == remainingLength) {
+				return i - lps[j - 1];
+			}
+			j = lps[(j - 1) < 0 ? 0 : j - 1];
+			remainingLength = m - j;
+		}
 		return -1;
 	}
 
 	/*
-	 * Finding length of longest suffix which is also a proper prefix for every
-	 * index in pattern
+	 * Finding length of longest proper suffix which is also a proper prefix for
+	 * every index in pattern.
+	 * 
+	 * Complexity: O(m) where m is length of pattern
 	 */
 	private int[] computeLPS(String pattern) {
 		int[] lps = new int[pattern.length()];
-
+		lps[0] = 0;
+		for (int i = 1; i < lps.length; i++) {
+			if (pattern.charAt(lps[i - 1]) == pattern.charAt(i)) {
+				lps[i] = lps[i - 1] + 1;
+			} else if (pattern.charAt(i - 1) == pattern.charAt(i)) {
+				lps[i] = lps[i - 1];
+			} else {
+				lps[i] = 0;
+			}
+		}
 		return lps;
 	}
 
--- src/test/java/com/learning/ads/string/patternmatch/KMPPatternMatchingTest.java
+++ src/test/java/com/learning/ads/string/patternmatch/KMPPatternMatchingTest.java
@@ -1,7 +1,33 @@
 package com.learning.ads.string.patternmatch;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class KMPPatternMatchingTest {
-	
-	
+
+	private KMPPatternMatching kmpPatternMatching = new KMPPatternMatching();
+
+	@Test
+	public void search() {
+		String text = "Hello there! Hope all are good and doing well";
+
+		// match at start
+		assertEquals(0, kmpPatternMatching.search("ABABDABACDABABCABAB", "ABABDABA"));
+		assertEquals(0, kmpPatternMatching.search(text, "H"));
+		assertEquals(35, kmpPatternMatching.search(text, "doing well"));
+		assertEquals(-1, kmpPatternMatching.search(text, "Hi Hello there!"));
+		// match at middle
+		assertEquals(6, kmpPatternMatching.search("ABABDABACDABABCABAB", "BACDABAB"));
+		assertEquals(18, kmpPatternMatching.search(text, "all are good"));
+		assertEquals(-1, kmpPatternMatching.search(text, "good are all"));
+		// match at end
+		assertEquals(15, kmpPatternMatching.search("ABCDABCXABXABCDABCDABCY", "ABCDABCY"));
+		assertEquals(11, kmpPatternMatching.search("ABABDABACDABABCABAB", "BABCABAB"));
+		assertEquals(5, kmpPatternMatching.search("ABABCD", "D"));
+		assertEquals(35, kmpPatternMatching.search(text, "doing well"));
+		assertEquals(-1, kmpPatternMatching.search(text, "doing well yeah"));
+
+	}
 
 }
