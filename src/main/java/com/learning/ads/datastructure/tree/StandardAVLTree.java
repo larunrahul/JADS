483bf093fcf1f751b24009ba568da5541a009f43
larunrahul <rahul.arun86@gmail.com>
2018-09-04 12:52:42 +0530

    Adding insert operation for AVL tree

---
+++ src/main/java/com/learning/ads/datastructure/tree/StandardAVLTree.java
@@ -0,0 +1,396 @@
+package com.learning.ads.datastructure.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.learning.ads.datastructure.queue.Queue;
+
+/**
+ * 
+ * One of the balanced implementation of BST.
+ * 
+ * Height property of ALV tree: Heights of left and right sub trees of every
+ * tree should differ by at most 1
+ * 
+ * Most of the operations can be done in O(log n) time due to the fact that tree
+ * is always balanced.
+ * 
+ * The height of the tree is at most log n where n is the number of nodes in
+ * tree.
+ * 
+ * Inorder traversal always results in sorted list of all nodes in tree
+ * 
+ * @author Arun Rahul
+ *
+ * @param <T>
+ */
+public class StandardAVLTree<T extends Comparable<T>> {
+
+	private Node<T> root;
+
+	private static class Node<T> {
+
+		public T value;
+		public Node<T> left;
+		public Node<T> right;
+		public Node<T> parent;
+		public int height;
+
+		public Node(T value) {
+			this.value = value;
+		}
+
+		public String toString() {
+			return value + " ";
+		}
+	}
+
+	public int height() {
+		return height(root);
+	}
+
+	private int height(Node<T> node) {
+		if (node == null) {
+			return 0;
+		}
+		return node.height;
+	}
+
+	public int depth() {
+		return height();
+	}
+
+	/**
+	 * Complexity: O(log n)
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public Node<T> search(T key) {
+		Node<T> node = root;
+		int comp;
+		while (node != null && (comp = key.compareTo(node.value)) != 0) {
+			if (comp < 0) {
+				node = node.left;
+			} else {
+				node = node.right;
+			}
+		}
+		return node;
+	}
+
+	/**
+	 * Complexity: O(log n)
+	 * 
+	 * @return
+	 */
+	public T min() {
+		Node<T> min = min(root);
+		return min == null ? null : min.value;
+	}
+
+	/**
+	 * Complexity: O(log n)
+	 * 
+	 * @return
+	 */
+	private Node<T> min(Node<T> node) {
+		Node<T> parent = null;
+		while (node != null) {
+			parent = node;
+			node = node.left;
+		}
+		return parent;
+	}
+
+	/**
+	 * Complexity: O(log n)
+	 * 
+	 * @return
+	 */
+	public T max() {
+		Node<T> max = max(root);
+		return max == null ? null : max.value;
+	}
+
+	/**
+	 * Complexity: O(log n)
+	 * 
+	 * @return
+	 */
+	private Node<T> max(Node<T> node) {
+		Node<T> parent = null;
+		while (node != null) {
+			parent = node;
+			node = node.right;
+		}
+		return parent;
+	}
+
+	/**
+	 * Complexity: O(log n)
+	 * 
+	 * @return
+	 */
+	public Node<T> successor(Node<T> node) {
+		Node<T> successor = node;
+		if (node.right != null) {
+			successor = min(node.right);
+		} else {
+			successor = node.parent;
+			while (successor != null && node.equals(successor.right)) {
+				node = successor;
+				successor = successor.parent;
+			}
+		}
+		return successor;
+	}
+
+	/**
+	 * Complexity: O(log n)
+	 * 
+	 * @return
+	 */
+	public Node<T> predecessor(Node<T> node) {
+		Node<T> successor = node;
+		if (node.left != null) {
+			successor = max(node.right);
+		} else {
+			successor = node.parent;
+			while (successor != null && node.equals(successor.left)) {
+				node = successor;
+				successor = successor.parent;
+			}
+		}
+		return successor;
+	}
+
+	/**
+	 * Complexity: O(log n)
+	 * 
+	 * @return
+	 */
+	public void insert(T key) {
+		root = insert(root, key);
+	}
+
+	private Node<T> insert(Node<T> node, T value) {
+		if (node == null) {
+			return new Node<T>(value);
+		}
+		int compValue = node.value.compareTo(value);
+		if (compValue > 0) {
+			node.left = insert(node.left, value);
+			node.left.parent = node;
+		} else {
+			node.right = insert(node.right, value);
+			node.right.parent = node;
+		}
+		node.height = Math.max(height(node.left), height(node.right)) + 1;
+		return balance(node);
+	}
+
+	private Node<T> balance(Node<T> x) {
+		if (Math.abs(height(x.left) - height(x.right)) <= 1) {
+			return x;
+		}
+		if (height(x.left) > height(x.right)) {
+			if (height(x.left.left) > height(x.left.right)) {
+				return rightRotate(x);
+			} else {
+				return leftRotate(x.left);
+			}
+		} else {
+			if (height(x.right.left) > height(x.right.right)) {
+				return rightRotate(x.right);
+			} else {
+				return leftRotate(x);
+			}
+		}
+	}
+
+	/**
+	 * This method presumes that right child of input node is not null
+	 * 
+	 * @param x
+	 */
+	private Node<T> leftRotate(Node<T> x) {
+		Node<T> y = x.right;
+		x.right = y.left;
+		if (y.left != null) {
+			y.left.parent = x;
+		}
+		y.parent = x.parent;
+		if (x.parent == null) {
+			this.root = y;
+		} else if (x == x.parent.left) {
+			x.parent.left = y;
+		} else {
+			x.parent.right = y;
+		}
+		y.left = x;
+		x.parent = y;
+		x.height = Math.max(height(x.left), height(x.right)) + 1;
+		y.height = Math.max(height(y.left), height(y.right)) + 1;
+		return y;
+	}
+
+	/**
+	 * This method presumes that left child of input node is not null
+	 * 
+	 * @param x
+	 */
+	private Node<T> rightRotate(Node<T> x) {
+		Node<T> y = x.left;
+		x.left = y.right;
+		if (y.right != null) {
+			y.right.parent = x;
+		}
+		y.parent = x.parent;
+		if (x.parent == null) {
+			this.root = y;
+		} else if (x == x.parent.left) {
+			x.parent.left = y;
+		} else {
+			x.parent.right = y;
+		}
+		y.right = x;
+		x.parent = y;
+		x.height = Math.max(height(x.left), height(x.right)) + 1;
+		y.height = Math.max(height(y.left), height(y.right)) + 1;
+		return y;
+	}
+
+	/**
+	 * Complexity: O(log n) but quickly grows to O(n) if tree becomes Skew tree
+	 * 
+	 * @return
+	 */
+	public void delete(T key) {
+		Node<T> node = search(key);
+		if (node == null) {
+			return;
+		}
+		if (node.left == null) {
+			transplant(node, node.right);
+		} else if (node.right == null) {
+			transplant(node, node.left);
+		} else {
+			Node<T> replacement = min(node.right);
+			if (!replacement.parent.equals(node)) {
+				transplant(replacement, replacement.right);
+				replacement.right = node.right;
+				replacement.right.parent = replacement;
+			}
+			transplant(node, replacement);
+			replacement.left = node.left;
+			replacement.left.parent = replacement;
+		}
+	}
+
+	private void transplant(Node<T> oTree, Node<T> rTree) {
+		if (oTree.parent == null) {
+			this.root = rTree;
+		} else if (oTree.equals(oTree.parent.left)) {
+			oTree.parent.left = rTree;
+		} else {
+			oTree.parent.right = rTree;
+		}
+		if (rTree != null) {
+			rTree.parent = oTree.parent;
+		}
+	}
+
+	/**
+	 * Complexity: O(n)
+	 * 
+	 * @return
+	 */
+	public List<T> traverseLevelOrder() {
+		List<T> list = new ArrayList<>();
+		Queue<Node<T>> queue = new Queue<>();
+		traverseLevelOrderRecursive(root, list, queue);
+		return list;
+	}
+
+	private void traverseLevelOrderRecursive(Node<T> node, List<T> list, Queue<Node<T>> queue) {
+		if (node == null) {
+			return;
+		}
+		list.add(node.value);
+		if (node.left != null) {
+			queue.enQueue(node.left);
+		}
+		if (node.right != null) {
+			queue.enQueue(node.right);
+		}
+		if (!queue.isEmpty()) {
+			traverseLevelOrderRecursive(queue.deQueue(), list, queue);
+		}
+	}
+
+	/**
+	 * Complexity: O(n)
+	 * 
+	 * @return
+	 */
+	public List<T> traverseInOrder() {
+		List<T> list = new ArrayList<>();
+		traverseInOrderRecursive(root, list);
+		return list;
+	}
+
+	private void traverseInOrderRecursive(Node<T> node, List<T> list) {
+		if (node == null) {
+			return;
+		}
+		traverseInOrderRecursive(node.left, list);
+		list.add(node.value);
+		traverseInOrderRecursive(node.right, list);
+	}
+
+	/**
+	 * Complexity: O(n)
+	 * 
+	 * @return
+	 */
+	public T[] traversePostOrder() {
+		List<T> list = new ArrayList<>();
+		traversePostOrderRecursive(root, list);
+		return listToArray(list);
+	}
+
+	private void traversePostOrderRecursive(Node<T> node, List<T> list) {
+		if (node == null) {
+			return;
+		}
+		traversePostOrderRecursive(node.left, list);
+		traversePostOrderRecursive(node.right, list);
+		list.add(node.value);
+	}
+
+	/**
+	 * Complexity: O(n)
+	 * 
+	 * @return
+	 */
+	public List<T> traversePreOrder() {
+		List<T> list = new ArrayList<>();
+		traversePreOrderRecursive(root, list);
+		return list;
+	}
+
+	private void traversePreOrderRecursive(Node<T> node, List<T> list) {
+		if (node == null) {
+			return;
+		}
+		list.add(node.value);
+		traversePreOrderRecursive(node.left, list);
+		traversePreOrderRecursive(node.right, list);
+	}
+
+	@SuppressWarnings("unchecked")
+	public T[] listToArray(List<T> list) {
+		return (T[]) list.toArray();
+	}
+}
---
+++ src/test/java/com/learning/ads/datastructure/tree/StandardAVLTreeTest.java
@@ -0,0 +1,42 @@
+package com.learning.ads.datastructure.tree;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StandardAVLTreeTest {
+
+	StandardAVLTree<Integer> tree;
+
+	@Before
+	public void setUp() throws Exception {
+		tree = new StandardAVLTree<>();
+	}
+
+	@Test
+	public void insert() {
+		tree.insert(50);
+		tree.insert(60);
+		tree.insert(70);
+		tree.insert(80);
+		tree.insert(90);
+		tree.insert(100);
+		tree.insert(110);
+		tree.insert(120);
+		tree.insert(130);
+		tree.insert(140);
+		tree.insert(150);
+		tree.insert(160);
+		tree.insert(170);
+		tree.insert(180);
+		tree.insert(190);
+		tree.insert(200);
+		assertEquals(4, tree.height());
+		System.out.println(tree.height());
+		assertArrayEquals(new Integer[] { 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200 },
+				tree.traverseInOrder().toArray());
+	}
+
+}
