a17147a96fc839187100e1f23c3ee2590dffbc4c
larunrahul <rahul.arun86@gmail.com>
2018-10-03 22:56:45 +0530

    0/1 Knapsack with top down dp approach

--- src/main/java/com/learning/ads/dynamicprogramming/Knapsack.java
+++ src/main/java/com/learning/ads/dynamicprogramming/Knapsack.java
@@ -1,20 +1,91 @@
 package com.learning.ads.dynamicprogramming;
 
+import java.util.Arrays;
+
+/**
+ * Standard binary (0/1) knapsack problem
+ * 
+ * https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+ * 
+ * @author Arun Rahul
+ *
+ */
 public class Knapsack {
 
+	/**
+	 * Complexity: O(2^N) where N is the number of weights
+	 * 
+	 * @param values
+	 * @param weights
+	 * @param capacity
+	 * @return
+	 */
 	public int binaryRec(int[] values, int[] weights, int capacity) {
-		return binaryRecr(values, weights, capacity, 0, 0);
+		return binaryRecr(values, weights, capacity, 0);
 	}
 
-	private int binaryRecr(int[] values, int[] weights, int capacity, int currentValue, int index) {
-		if (capacity == 0) {
-			return currentValue;
-		}
-		if (index >= weights.length) {
+	private int binaryRecr(int[] values, int[] weights, int capacity, int index) {
+		if (index == weights.length || capacity == 0) {
 			return 0;
 		}
-		return Math.max(binaryRecr(values, weights, capacity - weights[index], currentValue + values[index], index + 1),
-				binaryRecr(values, weights, capacity, currentValue, index + 1));
+		if (weights[index] > capacity) {
+			return binaryRecr(values, weights, capacity, index + 1);
+		}
+		return Math.max(values[index] + binaryRecr(values, weights, capacity - weights[index], index + 1),
+				binaryRecr(values, weights, capacity, index + 1));
+	}
+
+	/**
+	 * Complexity: O(N*C) where N is number of weights and C is the capacity of
+	 * knapsack
+	 * 
+	 * @param values
+	 * @param weights
+	 * @param capacity
+	 * @return
+	 */
+	public int binaryRecTopDown(int[] values, int[] weights, int capacity) {
+		int[][] dp = new int[weights.length + 1][capacity + 1];
+		for (int i = 0; i < dp.length; i++) {
+			Arrays.fill(dp[i], -1);
+		}
+		return binaryRecTopDown(values, weights, dp, capacity, 0);
+	}
+
+	private int binaryRecTopDown(int[] values, int[] weights, int[][] dp, int capacity, int index) {
+		if (dp[index][capacity] != -1) {
+			return dp[index][capacity];
+		}
+		int result;
+		if (index == weights.length || capacity == 0) {
+			result = 0;
+		} else if (weights[index] > capacity) {
+			result = binaryRecTopDown(values, weights, dp, capacity, index + 1);
+		} else {
+			result = Math.max(
+					values[index] + binaryRecTopDown(values, weights, dp, capacity - weights[index], index + 1),
+					binaryRecTopDown(values, weights, dp, capacity, index + 1));
+		}
+		return dp[index][capacity] = result;
+	}
+
+	/**
+	 * Complexity: O(N*C) where N is number of weights and C is the capacity of
+	 * knapsack
+	 * 
+	 * @param values
+	 * @param weights
+	 * @param capacity
+	 * @return
+	 */
+	public int binaryRecBottomUp(int[] values, int[] weights, int capacity) {
+		int[][] dp = new int[weights.length + 1][capacity + 1];
+		for (int i = 0; i < dp.length; i++) {
+			for (int j = 0; j <= capacity; j++) {
+
+			}
+		}
+		return dp[0][capacity];
 	}
 
 }
--- src/test/java/com/learning/ads/dynamicprogramming/KnapsackTest.java
+++ src/test/java/com/learning/ads/dynamicprogramming/KnapsackTest.java
@@ -1,6 +1,6 @@
 package com.learning.ads.dynamicprogramming;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,8 +15,15 @@ public class KnapsackTest {
 	}
 
 	@Test
-	public void binaryRecr() {
-		System.out.println(knapsack.binaryRec(new int[] { 60, 100, 120 }, new int[] { 10, 20, 30 }, 50));
+	public void binaryRec() {
+		assertEquals(220, knapsack.binaryRec(new int[] { 60, 100, 120 }, new int[] { 10, 20, 30 }, 50));
+		assertEquals(90, knapsack.binaryRec(new int[] { 10, 40, 30, 50 }, new int[] { 5, 4, 6, 3 }, 10));
+	}
+
+	@Test
+	public void binaryRecTopDown() {
+		assertEquals(220, knapsack.binaryRecTopDown(new int[] { 60, 100, 120 }, new int[] { 10, 20, 30 }, 50));
+		assertEquals(90, knapsack.binaryRecTopDown(new int[] { 10, 40, 30, 50 }, new int[] { 5, 4, 6, 3 }, 10));
 	}
 
 }
